7361985facba3fb725c9ab8ba3c596a116a26fe9
TamNTTc <TamNTTc@bkav.com>
2023-12-15 15:56:06 +0700

    build connection pool

---
+++ src/main/java/com/devtam/DevShop/Connection/ConnectionPool.java
@@ -0,0 +1,13 @@
+package com.devtam.DevShop.Connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionPool {
+    //xin kết nối để làm việc
+    public Connection getConnection(String objectName) throws SQLException;
+
+    //yêu cầu trả về kết nối
+    public void releaseConnection(Connection con, String objectName) throws SQLException;
+
+}
--- src/main/java/com/devtam/DevShop/WebConfiguration.java
+++ src/main/java/com/devtam/DevShop/WebConfiguration.java
@@ -27,4 +27,6 @@ public class WebConfiguration {
         return new DefaultServletHttpRequestHandler();
     }
 
+
+
 }
